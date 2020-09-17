ab13f631c5df865c9647da065da61ee3c9aab1f6
Matheus Sousa de Jesus <matheuss.jesus2017@gmail.com>
2020-09-17 01:45:39 -0300

    moving input

--- client/js/controller/BinController.js
+++ client/js/controller/BinController.js
@@ -1,29 +1,39 @@
-class BinController{
-    constructor(){
+class BinController {
+    constructor() {
         let $ = document.querySelector.bind(document);
+
         this._inputBin = $('#bin');
         this._buttonConvert = $('#convert');
         this._inputDec = $('#dec');
 
-        this._inputDec.disabled = true;
-        this._buttonConvert.onclick = (event)=>{this.convert(event)};
+        this._disabledInput(this._inputDec);
+
+        this._inputBin.oninput = () =>{this._validadeInput(this._inputBin.value,/^[0-1.]+$/);}
+        this._buttonConvert.onclick = (event) => { this.convert(event) };
     }
 
-    convert(event){
-       event.preventDefault();
+    convert(event) {
+        event.preventDefault();
+        let binariesNumbers = this._inputBin.value.split('').map((a) => parseInt(a));
+        console.log(binariesNumbers);
+        this._inputDec.value = this._convertsBinToDecimal(binariesNumbers);
 
-       let binariesNumbers = this._inputBin.value.split('').map((a)=>parseInt(a));
-       console.log(binariesNumbers);
-       this._inputDec.value = this._convertsBinToDecimal(binariesNumbers);       
-        
     }
-    _convertsBinToDecimal(binaries){
+    _convertsBinToDecimal(binaries) {
         let decimal = 0;
-         binaries.forEach((bin,index) => {
-            if(bin % 1 === 0) decimal += (Math.pow(2,index))
+        binaries.forEach((bin, index) => {
+            if (bin % 1 === 0) decimal += (Math.pow(2, index))
         });
-       
+        
         return decimal;
     }
-   
+    _validadeInput(input,exp) {
+        if(exp.exec(input) === null){
+            console.log(input)
+            this._inputBin.value = input.slice(0, -1);
+        }
+    }
+    _disabledInput(input){
+        input.disabled = true;
+    }
 }
