6a6c8a4a2bf8f18fe061d1485a39fe91e24b234d
Matheus Sousa de Jesus <matheuss.jesus2017@gmail.com>
2020-09-17 21:38:31 -0300

    working on the layout

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+app.use(express.static('client'))
 
 app.get("/", function(req,res){
     res.sendfile(__dirname +  '/client/index.html')
